e9597837e4751bb766d501107c1eb35d6063a0b2
clover <370546391@qq.com>
2017-06-28 22:08:51 +0800

    修改 ：HttpServer 自带 $_SERVER

--- src/HttpServer.php
+++ src/HttpServer.php
@@ -13,20 +13,22 @@ use Joking\Http\Functions\Parameters;
 
 class HttpServer extends Parameters {
 
+    public function __construct(array $parameters = array()) {
+        parent::__construct(empty($parameters) ? $_SERVER : $parameters);
+    }
+
     /**
      * Gets the HTTP headers.
      *
      * @return array
      */
-    public function getHeaders()
-    {
+    public function getHeaders() {
         $headers = array();
         $contentHeaders = array('CONTENT_LENGTH' => true, 'CONTENT_MD5' => true, 'CONTENT_TYPE' => true);
         foreach ($this->parameters as $key => $value) {
             if (0 === strpos($key, 'HTTP_')) {
                 $headers[substr($key, 5)] = $value;
-            }
-            // CONTENT_* are not prefixed with HTTP_
+            } // CONTENT_* are not prefixed with HTTP_
             elseif (isset($contentHeaders[$key])) {
                 $headers[$key] = $value;
             }
@@ -85,7 +87,7 @@ class HttpServer extends Parameters {
 
         // PHP_AUTH_USER/PHP_AUTH_PW
         if (isset($headers['PHP_AUTH_USER'])) {
-            $headers['AUTHORIZATION'] = 'Basic '.base64_encode($headers['PHP_AUTH_USER'].':'.$headers['PHP_AUTH_PW']);
+            $headers['AUTHORIZATION'] = 'Basic ' . base64_encode($headers['PHP_AUTH_USER'] . ':' . $headers['PHP_AUTH_PW']);
         } elseif (isset($headers['PHP_AUTH_DIGEST'])) {
             $headers['AUTHORIZATION'] = $headers['PHP_AUTH_DIGEST'];
         }
